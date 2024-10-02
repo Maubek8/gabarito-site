e69cda3742b8cccfdfd272364734aa536f6f6c8f
Maubek8 <mauriciokraemer@gmail.com>
2024-10-02 14:47:11 -0300

    Create camera.js

---
+++ js/camera.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Variáveis globais
+    const gabaritoMessage = document.getElementById("gabaritoMessage");
+    const alunosMessage = document.getElementById("alunosMessage");
+    const correctionMessage = document.getElementById("correctionMessage");
+
+    // Gabarito - Passo 1
+    const numQuestionsInput = document.getElementById("numQuestions");
+    const questionsInputDiv = document.getElementById("questionsInput");
